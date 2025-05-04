46cc74c90cb98c472a16c03cb696109cfccd3134
Rana <164146726+Ranayh14@users.noreply.github.com>
2025-05-04 19:53:57 +0700

    Delete mindsharehub directory

--- mindsharehub/config/admin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'email' => env('ADMIN_EMAIL', 'admin@example.com'),
-    'password' => env('ADMIN_PASSWORD', '12345678'),
-];
--- mindsharehub/resources/js/ziggy.js
+++
@@ -1,4 +0,0 @@
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-  Object.assign(Ziggy.routes, window.Ziggy.routes);
-}
-export { Ziggy };
